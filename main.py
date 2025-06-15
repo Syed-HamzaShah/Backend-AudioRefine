33b5cd613c11d0f5f94275442247436d15b6624a
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-06-15 17:14:16 +0500

    Ready for deployment

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
+
+
 from fastapi import FastAPI, File, UploadFile, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
