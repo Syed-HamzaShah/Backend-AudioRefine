31b54cf867a5a6cb5d662898d4a255de76bb66b4
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-06-15 18:57:32 +0500

    6.57

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 # utils/__init__.py
 from .spleeter_utils import run_spleeter
-from .rnnoise_utils import run_rnnoise
+# from .rnnoise_utils import run_rnnoise
 from .demucs_utils import run_demucs
 from .silero_utils import run_silero_vad
